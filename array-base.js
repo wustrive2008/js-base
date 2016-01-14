634a120114357276eb48be5bebd50fc5255597e2
wubaoguo <wustrive_2008@126.com>
2016-01-14 23:56:15 +0800

    去掉注释

--- array-base.js
+++ array-base.js
@@ -2,7 +2,7 @@
 
 //Array.prototype.join 将数组转为字符串
 
-/*
+
 var arr = [1,2,3];
 console.log(arr.join());
 console.log(arr.join("_"));
@@ -16,19 +16,19 @@ console.log(new Array(2).join("js"));
 console.log(repeatString("a.",4));
 console.log(repeatString("hello_",6));
 
-*/
+
 
 //Array.prototype.reverse 逆序
 
-/*
+
 var arr = [1,2,3,4,5];
 arr.reverse();
 console.log(arr);
-*/
+
 
 //Array.prototype.sort 排序
 
-/*
+
 var arr = ['c','a','b','e'];
 var sortArr = arr.sort();
 console.log(arr);
@@ -53,11 +53,11 @@ arr.forEach(function(item){
     console.log('age',item.age);
 });
 
-*/
+
 
 //Array.prototype.concat 合并数组
 
-/*
+
 var arr = [1,2,3];
 console.log(arr.concat(4,5));
 console.log(arr);
@@ -66,11 +66,11 @@ console.log(arr);
 console.log(arr.concat([12,33],14));
 console.log(arr.concat([1,2,3,[4,5]]));
 
-*/
+
 
 //Array.prototype.slice 截取数组
 
-/*
+
 //右不包含
 var arr = [1,2,3,4,5,6];
 console.log(arr.slice(1,4));
@@ -78,11 +78,11 @@ console.log(arr.slice(-1));
 console.log(arr.slice(-4,4));
 console.log(arr.slice(-4,-1));
 
-*/
+
 
 //Array.prototype.splice 数组拼接
 
-/*
+
 //删除元素
 var arr = [1,2,3,4,5];
 console.log(arr.splice(2));
@@ -97,22 +97,22 @@ arr = [1,2,3,4,5];
 console.log(arr.splice(1,1,'a','b'));
 console.log(arr);
 
-*/
+
 
 
 //Array.prototype.forEach 遍历
 
-/*
+
 //x:值  index:索引 a:数组本身
 var arr = [1,2,3,4,5];
 arr.forEach(function(x,index,a){
     console.log(x + '|' + index + '|' + (a === arr));
 });
-*/
+
 
 //Array.prototype.map 映射
 
-/*
+
 //原数组不变
 var arr = [1,2,3,4,5];
 var mapArr = arr.map(function(x){
@@ -121,11 +121,11 @@ var mapArr = arr.map(function(x){
 
 console.log(mapArr);
 console.log(arr);
-*/
+
 
 //Array.prototype.filter  过滤
 
-/*
+
 var arr = [1,2,3,4,5,6,7];
 var filterArr = arr.filter(function(x,index){
     return index % 2 === 0 || x > 4
@@ -134,11 +134,11 @@ var filterArr = arr.filter(function(x,index){
 console.log(filterArr);
 console.log(arr);
 
-*/
+
 
 //Array.prototype.every & some  数组判断
 
-/*
+
 var arr = [1,2,3,4,5];
 //判断是否所有元素都小于10
 console.log(arr.every(function(x){
@@ -161,13 +161,13 @@ console.log(arr.some(function(x){
     return x === 10;
 }));
 
-*/
+
 
 
 
 //Array.prototype.reduce & reduceRight  元素之间运算
 
-/*
+
 var arr = [1,2,3];
 //0+1 1+2 3+3
 var sum = arr.reduce(function(x,y){
@@ -194,12 +194,12 @@ var max = arr.reduceRight(function(x,y){
 
 console.log(max);
 
-*/
+
 
 
 //Array.prototype.indexOf&lastIndexOf 检索
 
-/*
+
 var arr = [1,2,3,4,1,2];
 console.log(arr.indexOf(2));
 console.log(arr.indexOf(10));
@@ -210,8 +210,16 @@ console.log(arr.indexOf(2,-1));
 console.log(arr.lastIndexOf(2));
 console.log(arr.lastIndexOf(2,-2));
 
-*/
 
 
 
-//Array.prototype.
+
+//Array.isArray 判断是否是数组
+
+console.log(Array.isArray([]));
+
+console.log([] instanceof Array);
+
+console.log(({}).toString.apply([]) === '[object Array]');
+
+console.log([].constructor === Array);
